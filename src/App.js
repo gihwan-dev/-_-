20762318a4d8fb6fe93082b9580229b78bca21d9
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-03-31 20:42:50 +0900

    feat(helmet): 라이브러리 헬멧 사용해 검색엔진 최적화
    - 트위터 메타태그 추가
    - 오픈그래프 메타태그 추가
    - 타이틀, 설명 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>항해 플러스 리액트 SEO 테스트.</title>
+        <meta
+          name="description"
+          content={"항해 플러스 리액트 SEO 적용 과제 솔루션 입니다."}
+        />
+        <meta
+          property="og:title"
+          content={"오픈 그래프 타이틀."}
+        />
+        <meta
+          property="og:description"
+          content={"오픈 그래프 설명"}
+        />
+        <meta
+          property="og:image"
+          content={
+            "https://plus.unsplash.com/premium_photo-1686149758342-9f0f249f2989?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
+          }
+        />
+        <meta
+          property="og:url"
+          content={""}
+        />
+        <meta
+          property="og:type"
+          content="website"
+        />
+        <meta
+          name="twitter:title"
+          content={"트위터 타이틀"}
+        />
+        <meta
+          name="twitter:description"
+          content={"트위터 설명"}
+        />
+        <meta
+          name="twitter:image"
+          content={
+            "https://plus.unsplash.com/premium_photo-1686149758342-9f0f249f2989?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
+          }
+        />
+        <meta
+          name="twitter:card"
+          content={
+            "https://plus.unsplash.com/premium_photo-1686149758342-9f0f249f2989?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8"
+          }
+        />
+      </Helmet>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
